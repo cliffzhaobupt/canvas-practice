6d98fafdf54d2c196c903f2e358780d725c4d5e0
Cliff Zhao <zty@Cliffs.local>
2015-07-27 01:27:34 +0900

    initial comment

---
+++ script/main-drawing.js
@@ -0,0 +1,35 @@
+(function (){
+  function init() {
+    var sakuraDrawing = document.getElementById('sakuraDrawing');
+    window.requestAnimationFrame(draw);
+  }
+
+  function draw() {
+    var mainDrawing = document.getElementById('mainDrawing');
+
+    if (mainDrawing.getContext) {
+      var context = mainDrawing.getContext('2d');
+      var time = new Date();
+
+      context.save();
+
+      context.clearRect(0, 0, 200, 200);
+      context.translate(100, 100);
+      context.rotate(((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds());
+
+      for (var i = 0 ; i < 16 ; i ++) {
+        var angle = Math.PI * 2 * i / 16;
+        var radius = 75;
+        var sakuraX = Math.sin(angle) * radius - 25;
+        var sakuraY = Math.cos(angle) * radius - 24;
+        context.drawImage(sakuraDrawing, sakuraX, sakuraY);
+      }
+
+      context.restore();
+      window.requestAnimationFrame(draw);
+
+    }
+  }
+
+  init();
+})();
---
+++ script/sakura-drawing.js
@@ -0,0 +1,29 @@
+(function (){
+  var sakuraImg = new Image();
+  function init() {
+    sakuraImg.src = 'image/sakura.png';
+    window.requestAnimationFrame(draw);
+  }
+
+  function draw() {
+    var sakuraDrawing = document.getElementById('sakuraDrawing');
+
+    if (sakuraDrawing.getContext) {
+      var context = sakuraDrawing.getContext('2d');
+      var time = new Date();
+
+      context.save();
+
+      context.clearRect(0, 0, 50, 48);
+      context.translate(25, 24);
+      context.rotate(((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds());
+      context.drawImage(sakuraImg, -15, -15);
+
+      context.restore();
+      window.requestAnimationFrame(draw);
+
+    }
+  }
+
+  init();  
+})();
